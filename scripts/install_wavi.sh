413ca3a94bf8e4fdfa1b1fa8fe6d85c8411fa91e
Alex Bradley <k2478354@erc-hpc-vmtmpl2026040921420411778.create.kcl.ac.uk>
2026-04-19 19:46:31 +0100

    install working

--- scripts/install_wavi.sh
+++ scripts/install_wavi.sh
@@ -2,7 +2,7 @@
 
 #
 # choose install location (!! This will overwrite anything you have in this depot, be careful !!)
-# # Script to install wavi 
+# # Script to install wavi. Need to first launch and interactive compute node session with at least 16gb memory: srun -p cpu --time 120 --mem=64000 --pty /bin/bash  (this example has 64gb)
 #
 #
 # choose install location (!! This will overwrite anything you have in this depot, be careful !!)
@@ -13,8 +13,8 @@ rm -rf $INSTALL_LOC
 mkdir $INSTALL_LOC
 echo "About to start installing wavi in $INSTALL_LOC"
 
-export SINGULARITYENV_JULIA_DEPOT_PATH="/opt/julia"
+export JULIA_DEPOT_PATH="/opt/julia"
+export JULIA_NUM_PRECOMPILE_TASKS=1
+export JULIA_CPU_THREADS=1
+
 singularity exec -B $INSTALL_LOC:/opt/julia,$(pwd) $IMGPATH julia julia_install_wavi.jl
-rm julia_install_wavi.jl
-echo "returning to $W_ROOT/scripts..."
-cd $W_ROOT/scripts/
---
+++ scripts/julia_install_wavi.jl
@@ -0,0 +1,5 @@
+#Script to install WAVI. Choose the branch you'd like to install using the rev argument, or leave blank for main
+
+using Pkg
+Pkg.add(url = "https://github.com/WAVI-ice-sheet-model/WAVI.jl")
+
